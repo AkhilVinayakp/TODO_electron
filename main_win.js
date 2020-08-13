91ed4fab18f5ba0cce925c24327bce1bd53fcd23
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-08-13 22:48:03 +0530

    Removing info functionality

--- main_win.js
+++ main_win.js
@@ -2,20 +2,21 @@ const electron = require('electron');
 const { ipcRenderer } = electron
 
 ipcRenderer.on('pushInfo',(event,data)=>{
-    document.getElementById("info").innerHTML=`<p>
-    <table>
-        <tr>
-        <td>ARCHITECTURE</td>
-        <td>${data.arch}</td>
-        </tr>
-        <tr>
-        <td>platform</td>
-        <td>${data.platform}</td>
-        </tr>
-        <tr>
-        <td>User</td>
-        <td>${data.user}</td>
-        </tr>
-    </table>
-</p>`
+//     document.getElementById("info").innerHTML=`<p>
+//     <table>
+//         <tr>
+//         <td>ARCHITECTURE</td>
+//         <td>${data.arch}</td>
+//         </tr>
+//         <tr>
+//         <td>platform</td>
+//         <td>${data.platform}</td>
+//         </tr>
+//         <tr>
+//         <td>User</td>
+//         <td>${data.user}</td>
+//         </tr>
+//     </table>
+// </p>`
+    document.getElementById("info").innerHTML = '<h2>hekk</h2>'
 });
