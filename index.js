e77b524e5913ca1b04cbb2432a297cf17c5fe9cb
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-08-13 22:21:10 +0530

    testing for menu action

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const electron = require('electron')
 const { app, BrowserWindow, Menu } = electron;
 let mainWindow;
-
+// menu Template
+const menuTemplate = [
+    {
+        label: 'file',
+        submenu:[
+            {
+                label: "new TODO"
+            },
+            {
+                label: "Quit",
+                click(){
+                    app.quit();
+                }
+            },
+            {
+                label: "about",
+                click() {
+                    mainWindow.webContents.send('pushInfo',{
+                        arch : process.arch,
+                        platform : process.platform,
+                        user: process.env.USER
+                    })
+                }
+            }
+        ]
+    },
+];
+if(process.platform === 'darvin'){
+    menuTemplate.unshift({});
+}
+// creating menubar from the template
+const mainMenu = Menu.buildFromTemplate(menuTemplate);
 
 app.on('ready',()=>{
     mainWindow = new BrowserWindow({
@@ -10,11 +41,5 @@ app.on('ready',()=>{
         }
     });
     mainWindow.loadURL(`file://${__dirname}/Templates/index.html`).then(r => console.log(r));
-    const mainMenu = Menu.buildFromTemplate(menuTemplate);
     Menu.setApplicationMenu(mainMenu);
 })
-const menuTemplate = [
-    {
-        label: 'file'
-    },
-];
