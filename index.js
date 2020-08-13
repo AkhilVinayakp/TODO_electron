6367bccb34fe100e37b2b87ed0b71ec1e240a103
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-08-13 21:05:00 +0530

    adding custom menu bar

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const electron = require('electron')
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 let mainWindow;
+
+
 app.on('ready',()=>{
     mainWindow = new BrowserWindow({
-
-    })
+        webPreferences:{
+            nodeIntegration:true
+        }
+    });
+    mainWindow.loadURL(`file://${__dirname}/Templates/index.html`).then(r => console.log(r));
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 })
+const menuTemplate = [
+    {
+        label: 'file'
+    },
+];
