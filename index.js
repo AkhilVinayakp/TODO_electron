94219a7625ab7733539b5a30aa5c600b131ceb5e
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-08-15 20:06:32 +0530

    menu swiching fixed

--- index.js
+++ index.js
@@ -3,31 +3,6 @@ const { app, BrowserWindow, Menu } = electron;
 let mainWindow;
 let addWindow;
 
-app.on('ready',()=>{
-    mainWindow = new BrowserWindow({
-        webPreferences:{
-            nodeIntegration:true
-        }
-    });
-    mainWindow.loadURL(`file://${__dirname}/Templates/index.html`).then(r => console.log(r));
-    // creating menubar from the template
-    const mainMenu = Menu.buildFromTemplate(menuTemplate);
-    Menu.setApplicationMenu(mainMenu);
-})
-
-// creating a new window
-function create_addWindow() {
-    addWindow = new BrowserWindow({
-        height:140,
-        width:400,
-        darkTheme:true,
-        maximizable:false,// linux won't work
-        minimizable:false,// linux won't work
-        autoHideMenuBar:true,
-        webPreferences:{nodeIntegration:true}
-    })
-    addWindow.loadURL(`file://${__dirname}/Templates/add.html`);
-}
 const menuTemplate = [
     {
         label: 'file',
@@ -58,4 +33,35 @@ const menuTemplate = [
 
 if(process.platform === 'darvin'){
     menuTemplate.unshift({});
+}
+const mainMenu = Menu.buildFromTemplate(menuTemplate);
+app.on('ready',()=>{
+    mainWindow = new BrowserWindow({
+        webPreferences:{
+            nodeIntegration:true
+        }
+    });
+    mainWindow.loadURL(`file://${__dirname}/Templates/index.html`).then(r => console.log(r));
+    // creating menubar from the template
+    // close all the windows if the main window is closed
+    mainWindow.on('closed',()=>app.quit());
+
+    Menu.setApplicationMenu(mainMenu);
+})
+
+// creating a new window
+function create_addWindow() {
+    addWindow = new BrowserWindow({
+        height:140,
+        width:400,
+        darkTheme:true,
+        maximizable:false,// linux won't work
+        minimizable:false,// linux won't work
+        autoHideMenuBar:true,
+        webPreferences:{nodeIntegration:true},
+        title:"ADD NEW TODO"
+    })
+    addWindow.loadURL(`file://${__dirname}/Templates/add.html`);
+    Menu.setApplicationMenu(null); //TODO main application menu disabled but not restored after exiting // DONE
+    addWindow.on('closed',()=> Menu.setApplicationMenu(mainMenu))
 }
